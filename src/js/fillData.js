ca8ba6d857fcf7d7d6c98750a83485c13a63c43b
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2024-06-06 16:34:40 +0400

    Add logic to display forecast data in their cards

--- src/js/fillData.js
+++ src/js/fillData.js
@@ -1,5 +1,5 @@
-/* global document */
-
+/*  global document */
+//  export at the very bottom
 function displayCurrentWeather(data) {
     const todayTab = document.querySelector('.todayTab');
     const details = document.querySelector('.details .addInfo');
@@ -18,6 +18,7 @@ function displayCurrentWeather(data) {
         data.current.condition.text;
     todayTab.querySelector('img').src = data.current.condition.icon;
 
+    // details tab
     details.querySelector('.feelsLike p+p').textContent =
         `${data.current.feelslike_c}`;
     details.querySelector('.wind p+p').textContent = `${data.current.wind_kph}`;
@@ -26,20 +27,47 @@ function displayCurrentWeather(data) {
     details.querySelector('.UV p+p').textContent = `${data.current.uv}`;
     details.querySelector('.visibility p+p').textContent =
         `${data.current.vis_km}`;
-    details.querySelector('.precipitation p+p').textContent =
-        `${data.current.precip_mm}`;
+    details.querySelector('.rainChance p+p').textContent =
+        `${data.forecast.forecastday[0].day.daily_chance_of_rain}`;
+}
+
+function displayForecastData(dayCard, data) {
+    const date = dayCard.querySelector('.date');
+    date.textContent = `${data.date}`;
+
+    const avgTemp = dayCard.querySelector('.temperature span');
+    avgTemp.textContent = `${data.day.avgtemp_c}`;
+
+    const minTemp = dayCard.querySelector('.minTemp span');
+    minTemp.textContent = `${data.day.mintemp_c}`;
+
+    const maxTemp = dayCard.querySelector('.maxTemp span');
+    maxTemp.textContent = `${data.day.maxtemp_c}`;
+
+    const condition = dayCard.querySelector('.condition');
+    condition.textContent = data.day.condition.text;
+
+    const icon = dayCard.querySelector('img');
+    icon.src = data.day.condition.icon;
+
+    const UVindex = dayCard.querySelector('.UV p+p');
+    UVindex.textContent = data.day.uv;
+
+    const rainChance = dayCard.querySelector('.rainChance p+p');
+    rainChance.textContent = data.day.daily_chance_of_rain;
+
+    const avgHumidity = dayCard.querySelector('.humidity p+p');
+    avgHumidity.textContent = data.day.avghumidity;
+
+    const totalPrecipitation = dayCard.querySelector('.totalPrecipitation p+p');
+    totalPrecipitation.textContent = data.day.totalprecip_mm;
 }
 
 export default function fillData(weatherData) {
+    // console.log(weatherData);
     displayCurrentWeather(weatherData);
-    console.log(weatherData);
-
-    /* weatherData.forecast.forecastday.forEach((date) => {
-        console.log(`Average temperature: ${date.day.avgtemp_c}`);
-        console.log(`Max temperature: ${date.day.maxtemp_c}`);
-        console.log(`Min temperature: ${date.day.mintemp_c}`);
-        console.log(`Average humidity: ${date.day.avghumidity} %`);
-        console.log(`Max wind speed: ${date.day.maxwind_kph} km/h`);
-        console.log(`UV: ${date.day.uv}`);
-    }); */
+    const forecastCards = document.querySelectorAll('.forecastTab > div');
+    forecastCards.forEach((card, index) => {
+        displayForecastData(card, weatherData.forecast.forecastday[index + 1]);
+    });
 }
